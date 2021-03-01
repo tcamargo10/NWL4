a0c988a25f35f60a6f2334ec7008d9cadfbfab0b
Thiago Camargo <thiago.camargo@outlook.com>
2021-02-28 21:15:29 -0300

    adicionado Countdown component

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react'
+import styles from '../styles/components/Countdown.module.css'
+
+export default function Countdown() {
+    const [time, setTime] = useState(25 * 60);
+    const [active, setActive] = useState(false);
+
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+
+    const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+    const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+    useEffect(() => {
+        
+    }, [active]);
+
+    function startCountdown() {
+        setActive(true);
+    }
+
+    return (
+        <div>
+            <div className={styles.CountdownContainer} >
+                <div>
+                    <span>{minuteLeft}</span>
+                    <span>{minuteRight}</span>
+                </div>
+                <span>:</span>
+                <div>
+                    <span>{secondLeft}</span>
+                    <span>{secondRight}</span>
+                </div>
+            </div>
+
+            <button type="button" className={styles.countdownButton} onClick={startCountdown}>
+                Iniciar um ciclo
+            </button>
+        </div>
+    )
+}
